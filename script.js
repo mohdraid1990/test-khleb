919d1b9b81e8c80200e3b9fd9999aed523e67f2a
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-07-28 20:07:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cartIcon = document.querySelector('.cart img');
+    let cartCount = 0;
+  
+    // إضافة `span` لعرض العدد الافتراضي "0" عند تحميل الصفحة.
+    const countSpan = document.createElement('span');
+    countSpan.classList.add('cart-count');
+    countSpan.textContent = cartCount;
+    cartIcon.parentNode.appendChild(countSpan);
+  
+    const updateCartCount = (newCount) => {
+        cartCount += newCount;
+        document.querySelector('.cart-count').textContent = cartCount;
+    };
+  
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', function() {
+            const quantity = parseInt(this.previousElementSibling.querySelector('.quantity').textContent, 10);
+            updateCartCount(quantity);
+        });
+    });
+  
+    document.querySelectorAll('.increase-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            const quantitySpan = this.previousElementSibling;
+            let quantity = parseInt(quantitySpan.textContent, 10);
+            quantity++;
+            quantitySpan.textContent = quantity;
+        });
+    });
+  
+    document.querySelectorAll('.decrease-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            const quantitySpan = this.nextElementSibling;
+            let quantity = parseInt(quantitySpan.textContent, 10);
+            if (quantity > 1) {
+                quantity--;
+                quantitySpan.textContent = quantity;
+            }
+        });
+    });
+  });
+  
